25f4f3c8be63a98c1b9043bcce8ab67bbb01d037
Joshua <mcabej12@gmail.com>
2021-02-02 11:30:19 +0000

    Update config.service.js
    https sslforfree.com

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://jrd-api-quiz-env.eba-d4qytphe.us-east-2.elasticbeanstalk.com/';
+    service.apiBase = 'https://jrd-api-quiz-env.eba-d4qytphe.us-east-2.elasticbeanstalk.com/';
 
     return service;
 }]);
